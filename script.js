12e1c9d3498d87805815a0f751dc2426736c7d68
jarod79 <arrijuria.eric@gmail.com>
2021-10-28 10:47:19 +0200

    test

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Variable for html 
-const formulaire = document.getElementById('my-form');
+// Variable for html
+const formulaire = document.getElementById("my-form");
 const playerName = document.getElementById(`player-name`);
 const numberPlayer = document.getElementById(`value`);
 const valid = document.getElementById(`valid`);
 const player = document.getElementById(`player`);
-const replay = document.getElementById('reset');
+const replay = document.getElementById("reset");
 
 // Style of Body
 body.style.backgroundImage = `url(https://www.transparenttextures.com/patterns/brushed-alum.png)`;
@@ -16,7 +16,6 @@ body.style.alignItems = `center`;
 body.style.textAlign = `center`;
 body.style.fontSize = `2rem`;
 
-
 // Style of form
 formulaire.style.width = `100vh`;
 formulaire.style.margin = `auto`;
@@ -24,21 +23,19 @@ formulaire.style.textAlign = `center`;
 formulaire.style.display = `flex`;
 formulaire.style.flexDirection = `column`;
 
-
 //Style of all input
 for (const entry of document.querySelectorAll(`input`)) {
   entry.style.borderRadius = `8px`;
-  entry.style.width = '50vh';
-  entry.style.height = '30px';
-  entry.style.textAlign = 'center';
-  entry.style.border = `2px solid Orange`
+  entry.style.width = "50vh";
+  entry.style.height = "30px";
+  entry.style.textAlign = "center";
+  entry.style.border = `2px solid Orange`;
   entry.style.margin = `auto`;
   entry.style.boxShadow = `5px 5px orange`;
 }
 //Style of number entry input
 numberPlayer.style.marginBottom = `20px`;
 numberPlayer.style.marginTop = `10px`;
-
 
 // Style of Button valid
 valid.style.color = `white`;
@@ -47,7 +44,7 @@ valid.style.border = `2px solid shadow`;
 valid.style.margin = `auto`;
 valid.style.width = `20vh`;
 valid.style.height = `25px`;
-valid.style.boxShadow = `5px 5px 5px black`
+valid.style.boxShadow = `5px 5px 5px black`;
 
 //Diplay of Name to player
 //Create h1
@@ -68,13 +65,12 @@ change.innerHTML = `Modify Name`;
 change.style.display = `none`;
 change.style.margin = `auto`;
 change.style.marginBottom = `5vh`;
-change.style.boxShadow = `5px 5px 5px black`
+change.style.boxShadow = `5px 5px 5px black`;
 
-
-// Function to hide the input entry 
-valid.addEventListener(`click`, event => {
+// Function to hide the input entry
+valid.addEventListener(`click`, (event) => {
   //diplay name of player
-  if (playerName.value !== '') {
+  if (playerName.value !== "") {
     playerName.style.display = `none`;
     displayName.innerHTML = `Let's start, ${playerName.value} ! `;
     change.style.display = `block`;
@@ -85,24 +81,21 @@ valid.addEventListener(`click`, event => {
     displayName.innerHTML = `Enter your name please !`;
   }
 
-
   //function to modified the name in party
-})
-change.addEventListener(`click`, event => {
+});
+change.addEventListener(`click`, (event) => {
   event.preventDefault();
   playerName.style.display = `block`;
-})
+});
 
 // Random number to find
 const random = Math.floor(Math.random() * 100) + 1;
-
 
 // Info for player to game instant
 //Create h1
 let resultat = document.createElement(`h1`);
 //PLace to HTML
 formulaire.appendChild(resultat);
-
 
 // Style of display Info
 resultat.style.fontSize = `4rem`;
@@ -112,25 +105,25 @@ resultat.style.color = `red`;
 let countTry = 0;
 
 // Function who calculate the result and write on html
-valid.addEventListener('click', event => {
+valid.addEventListener("click", (event) => {
   event.preventDefault();
   countTry++;
   console.log(random);
   if (numberPlayer.value < 1 || numberPlayer.value > 100) {
-    resultat.innerHTML = (`Type a number between 1 and 100!`);
+    resultat.innerHTML = `Type a number between 1 and 100!`;
   }
   // if number are more of the random.
   else if (numberPlayer.value < random) {
-    resultat.innerHTML = (`It is more than ${numberPlayer.value}`);
+    resultat.innerHTML = `It is more than ${numberPlayer.value}`;
     //reset entry to input
-    numberPlayer.value = '';
+    numberPlayer.value = "";
   }
 
   // if the number are less of the random.
   else if (numberPlayer.value > random) {
-    resultat.innerHTML = (`It is less than ${numberPlayer.value}`);
+    resultat.innerHTML = `It is less than ${numberPlayer.value}`;
     //reset entry to input
-    numberPlayer.value = '';
+    numberPlayer.value = "";
   }
 
   // If found the wonderfull number.
@@ -139,15 +132,13 @@ valid.addEventListener('click', event => {
     resultat.style.textShadow = `3px 1px 3px black`;
     numberPlayer.style.display = `none`;
     valid.innerHTML = `Replay`;
-    valid.addEventListener(`click`, event => {
+    valid.addEventListener(`click`, (event) => {
       window.location.reload(playerName);
     });
     playerName.style.display = `none`;
     displayName.style.display = `none`;
     change.style.display = `none`;
     body.style.backgroundImage = `url("https://www.transparenttextures.com/patterns/old-map.png")`;
-    resultat.innerHTML = (`Congratulation ${playerName.value} , you win in ${countTry} try !!!`);
+    resultat.innerHTML = `Congratulation ${playerName.value} , you win in ${countTry} try !!!`;
   }
-
-})
-
+});
